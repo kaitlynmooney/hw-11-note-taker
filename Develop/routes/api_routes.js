54765f5aac0111942d36e92805d98a10e6f7821e
kaitlynmooney <kmooney806@gmail.com>
2024-07-13 17:14:34 -0400

    added routes and functionality

---
+++ Develop/routes/api_routes.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+
+module.exports = function(app) {
+
+    app.get("/api/notes", function(req, res) {
+       
+        res.json(data);
+
+    });
+
+    app.get("/api/notes/:id", function(req, res) {
+
+        res.json(data[Number(req.params.id)]);
+
+    });
+
+
+    app.post("/api/notes", function(req, res) {
+
+        const newNote = req.body;
+        const uniqueId = (data.length).toString();
+        console.log(uniqueId);
+        newNote.id = uniqueId;
+        data.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
+            if (err) throw (err);        
+        }); 
+
+        res.json(data);    
+
+    });
+
+    
+    app.delete("/api/notes/:id", function(req, res) {
+
+        const noteId = req.params.id;
+        const newId = 0;
+        console.log(`Deleting note with id ${noteId}`);
+        data = data.filter(currentNote => {
+           return currentNote.id != noteId;
+        });
+        for (currentNote of data) {
+            currentNote.id = newId.toString();
+            newId++;
+        }
+        fs.writeFileSync("./db/db.json", JSON.stringify(data));
+        res.json(data);
+    }); 
+
+}
